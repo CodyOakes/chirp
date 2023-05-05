1c3c5e37912b30ecfae98e4be8d3340d786e882b
Cody Oakes <codyoakes@me.com>
2023-05-05 07:23:07 -0600

    routes profile post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ import type { RouterOutputs } from "~/utils/api";
 import toast from "react-hot-toast";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Link from "next/link";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -24,7 +25,6 @@ const CreatePostWizard = () => {
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
-      console.log(errorMessage);
       if (errorMessage && errorMessage[0]) {
         toast.error(errorMessage[0]);
       } else {
@@ -88,8 +88,13 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
-          <span>{`@${author.username}`}</span>·
-          <span>{`${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          ·
+          <Link href={`/post/${post.id}`}>
+            <span>{`${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
